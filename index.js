74b40cb61457951554d29787822c2d4be2283315
darlose04 <zsmith126028@gmail.com>
2020-05-01 07:59:08 -0500

    add simple message and reply action. add token

--- index.js
+++ index.js
@@ -2,3 +2,19 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 const axios = require("axios");
 require("dotenv").config();
+
+const prefix = "!";
+
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}`);
+});
+
+client.on("message", (msg) => {
+  if (msg.content === "ping") {
+    msg.reply("pong");
+  }
+});
+
+let token = process.env.TOKEN;
+
+client.login(token);
